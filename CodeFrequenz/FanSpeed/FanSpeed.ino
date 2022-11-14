d4f3ad528ab79e64d34672014189f54f2ea895b8
Natthaphon-j024 <0892690885j@gmail.com>
2022-11-15 05:07:06 +0700

    add folder code .ino

---
+++ CodeFrequenz/FanSpeed/FanSpeed.ino
@@ -0,0 +1,43 @@
+#include <TimerOne.h>
+
+// This example creates a PWM signal with 25 kHz carrier.
+//
+// Arduino's analogWrite() gives you PWM output, but no control over the
+// carrier frequency.  The default frequency is low, typically 490 or
+// 3920 Hz.  Sometimes you may need a faster carrier frequency.
+//
+// The specification for 4-wire PWM fans recommends a 25 kHz frequency
+// and allows 21 to 28 kHz.  The default from analogWrite() might work
+// with some fans, but to follow the specification we need 25 kHz.
+//
+// http://www.formfactors.org/developer/specs/REV1_2_Public.pdf
+//
+// Connect the PWM pin to the fan's control wire (usually blue).  The
+// board's ground must be connected to the fan's ground, and the fan
+// needs +12 volt power from the computer or a separate power supply.
+float sensorPin = A7;
+int sensorValue = 0;
+int ledPin = 3;  
+const int fanPin = 6;
+
+void setup(void)
+{
+  Timer1.initialize(40);  // 40 us = 25 kHz
+  Serial.begin(9600);
+}
+
+void loop(void)
+{
+  sensorValue = analogRead(sensorPin);
+  sensorValue = sensorValue /10.23;
+  
+  // slowly increase the PWM fan speed
+  //
+float dutyCycle = sensorValue ; 
+    analogWrite(ledPin,(sensorValue*2.55) );
+    Serial.print("PWM Fan, Duty Cycle = ");
+    Serial.println(dutyCycle);
+    Timer1.pwm(fanPin, (dutyCycle / 100) * 1023);
+    delay(500);
+ 
+}
